19e7c9ca5779d68c6be7ae5152274761078316dd
rathwaj <50718431+rathwaj@users.noreply.github.com>
2019-05-23 16:38:46 -0400

    Update Fsdd1.java

--- src/fsdd1/Fsdd1.java
+++ src/fsdd1/Fsdd1.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- May 23 2019*/
+ May 23 2019 sheridan college*/
 package fsdd1;
 
 import java.util.Scanner;
